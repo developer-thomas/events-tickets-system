dd0bc5820f6f8becdb4cd75be5c21827f7280b1f
Thomas Edson <andepipous@gmail.com>
2025-06-06 09:17:36 -0400

    chore: appointment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  // api: 'http://184.174.32.25:8045',
-  api: 'https://homolog.crosoften.com:8045',
+  // api: 'https://d68c-170-244-253-37.ngrok-free.app',
+  api: 'http://184.174.32.25:8045'
+  // api: 'https://homolog.crosoften.com:8045',
 };
