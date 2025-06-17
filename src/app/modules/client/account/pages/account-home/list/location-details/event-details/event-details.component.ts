302f1b785bc503fb19e1749bc0b4183ca74cfd66
Thomas Edson <andepipous@gmail.com>
2025-06-17 16:50:54 -0400

    feat: implement buy ticket action btn integration

--- src/app/modules/client/account/pages/account-home/list/location-details/event-details/event-details.component.ts
+++ src/app/modules/client/account/pages/account-home/list/location-details/event-details/event-details.component.ts
@@ -4,6 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EventDetailsGenericComponent } from '.././../../../../../../shared/components/event-details/event-details.component'
+import { AccountHomeService } from '../../../account-home.service';
+import { GetOneEvent } from '../../../../../../../gerencial/pages/event/models/GetEventById.interface';
+import { CartRequest } from '../../../models/AddToCart.interface';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-event-details',
@@ -15,6 +19,8 @@ import { EventDetailsGenericComponent } from '.././../../../../../../shared/comp
 export class EventDetailsComponent {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private accountHomeService = inject(AccountHomeService)
+  private toastr = inject(ToastrService);
 
   eventId!: any;
   isFavorite = false
@@ -35,7 +41,27 @@ export class EventDetailsComponent {
     console.log("Sharing event:", this.eventId)
   }
 
-  buyTicket(): void {
+  static getUserId() {
+    return localStorage.getItem('userId');
+  }
+
+  buyTicket(ticket: GetOneEvent): void {
+    const userId = Number(EventDetailsComponent.getUserId());
+  
+    const payload: CartRequest = {
+      userId,
+      item: {
+        eventId: ticket.id,
+        quantity: 1,
+        value: ticket.value
+      }
+    }
+    this.accountHomeService.addItemsToCart(payload).subscribe({
+      next: (res) => {
+        console.log('ingresso adicionado:', res)
+        this.toastr.success("Ingresso adicionado ao carrinho");
+      }
+    })
     this.router.navigate(['client/inicio/cart']);
   }
 }
